33d83710e285deb903590eb3ca6ca7dffcb59e7d
javiros <javier_rossetti1972@hotmail.com>
2014-04-10 22:50:24 +0100

    added chartsjs

---
+++ js/myCharts.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+	//Get context with jQuery - using jQuery's .get() method.
+	var ctx = $("#myChart").get(0).getContext("2d");
+	//This will get the first returned node in the jQuery collection.
+	var myNewChart = new Chart(ctx);
+
+	
+	var data = [
+	{
+		value: 30,
+		color:"#F38630"
+	},
+	{
+		value : 50,
+		color : "#E0E4CC"
+	},
+	{
+		value : 100,
+		color : "#69D2E7"
+	}			
+];
+var options = {
+    scaleFontColor: "#f00",
+    datasetStrokeWidth: 20
+};
+
+new Chart(ctx).Pie(data,options);
+});
