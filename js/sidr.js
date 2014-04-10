68b7e87396e5b9851b681500dd1f26e41bda100c
broonzy <ger_daly@yahoo.com>
2014-04-11 00:07:58 +0100

    added swipe for devices on navbar

--- js/sidr.js
+++ js/sidr.js
@@ -1,4 +1,4 @@
-
+ //source http://www.berriart.com/sidr/#development
 $(document).ready(function() {
     $('#simple-menu').sidr();
     });
@@ -7,4 +7,17 @@ $(document).ready(function() {
 $('#responsive-menu-button').sidr({
     name: 'sidr-main',
     source: '#navigation'
+});
+
+ // the touchwipe function for devices
+$(window).touchwipe({
+    wipeLeft: function() {
+// Close
+        $.sidr('close', 'sidr-main');
+    },
+    wipeRight: function() {
+// Open
+        $.sidr('open', 'sidr-main');
+    },
+    preventDefaultEvents: false
 });
