771807b4166ed7c2c8d7da0a624e29871ce06a1b
broonzy <ger_daly@yahoo.com>
2014-04-11 14:28:48 +0100

    all css aligned better and video is fixed top right smaller size

---
+++ js/canvas.js
@@ -0,0 +1,22 @@
+  /*  this is just a template chart. have moved to bottom of main youtube API request for testing
+window.onload = function() {
+    var chart = new CanvasJS.Chart("chartcontainer", {
+    title:{
+    text: "fruits sold in first quarter"
+    },
+data: [
+				{type: "column",
+                     dataPoints: [
+                     { label: "banana", y: 18 },
+                     { label: "orange", y: 29 },
+                     { label: "apple", y: 40 },
+                     { label: "mango", y: 34 },
+                     { label: "grape", y: 24 }
+                    ]
+                }
+      ]
+});
+chart.render();
+};
+
+  */
