9b4429b820042d4c12ba423fef99900424c1ca7a
paul <mannion.paul@gmail.com>
2014-04-12 15:32:30 +0100

    basic login page added - UserApp registerd and available for sign ups. public pages assigned to UserApp in App.js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
-angular.module('arial_ca3', [
+var app = angular.module('arial_ca3', [
         'ngRoute',
         'arial_ca3.controllers',
         'UserApp'
     ]).
     config(['$routeProvider', function($routeProvider) {
         $routeProvider
-            .when('/login', {templateUrl: 'login.html'})
-            .when('/signup', {templateUrl: 'signup.html'})
+            .when('/login', {templateUrl: 'login.html', public:true, login:true})
+            .when('/signup', {templateUrl: 'signup.html',public:true})
             .when('/dashboard', {templateUrl: 'dashboard.html', controller: 'dashboardCtrl'})
-            .otherwise({redirectTo: '/home'});
+            .otherwise({redirectTo: '/dashboard'});
     }])
     .run(function($rootScope,user) {
-
+        user.init({ appId: '5349161769376' });
     });
